6c72f3650cc5c5a0b565fdd6bf2d374200d37b42
Nisa Nur <nisanurbastimur@gmail.com>
2024-04-09 18:37:13 +0300

    Initial Commit

---
+++ LovelyMovieUI/LovelyMovieUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelyMovieUIApp.swift
+//  LovelyMovieUI
+//
+//  Created by Nisa Nur Baştimur on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LovelyMovieUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
